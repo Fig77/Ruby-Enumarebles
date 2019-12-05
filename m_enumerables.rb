25b25e4636485f85ceb855686a34dbdb51cfbfdd
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-05 17:25:31 -0300

    fighting with robocop

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -31,20 +31,18 @@ module Enumerable
     end
   end
 
-  def m_all?(*a)
+  def m_all?(_args)
     i = 0
     while i < length
      return false if block_given? && !yield(self[i])
-     return false if !self[i] || !(self[i] === a) # rubocop:disable Style/CaseEquality
+     return false if !self[i] || !(self[i] === _args) # rubocop:disable Style/CaseEquality
 
      i += 1
     end
     true
   end
 
-  def m_any?(*a)
+  def m_any?(_args)
     block_given? ? !m_all? { yield(self) } : !m_all?
   end
 end
-
-puts %w[ant bear cat].m_all?(/t/)
