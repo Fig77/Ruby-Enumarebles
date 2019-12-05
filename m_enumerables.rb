0fd346c112b7e51d39f48fd17ddaca039bdae065
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-05 13:28:34 -0300

    I should really set up something to test stickler locally

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -2,12 +2,11 @@
 
 module Enumerable
   def m_each
-  return self unless block_given?
-  
-    i = 0
-    while i < length
-      yield(self[i])
-        i += 1
-    end
+    return self unless block_given?
+      i = 0
+      while i < length
+        yield(self[i])
+          i += 1
+      end
   end
 end
