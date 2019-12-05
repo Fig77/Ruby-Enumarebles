d013888cb4267d316b18893c4d232913207bb041
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-05 13:24:37 -0300

    still fixing some indentation stuff..

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -3,10 +3,10 @@
 module Enumerable
   def m_each
     return self unless block_given?
-      i = 0
-      while i < length
-          yield(self[i])
-          i += 1
-      end
+    i = 0
+    while i < length
+      yield(self[i])
+        i += 1
+    end
   end
 end
