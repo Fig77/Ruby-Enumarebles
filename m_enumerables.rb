ab5c73d7e311e976e08d1911f546c72c2523a679
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-05 13:36:45 -0300

    I think I'm getting into a loop :D

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -3,10 +3,11 @@
 module Enumerable
   def m_each
     return self unless block_given?
+    
     i = 0
     while i < length
       yield(self[i])
-        i += 1
+      i += 1
     end
   end
 end
