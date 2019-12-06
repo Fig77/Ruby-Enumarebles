677be4bebe5636555eb37cbd293e943f43526358
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-06 13:50:56 -0300

    attempt to reduced complexity 1 more point

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -37,16 +37,28 @@ module Enumerable
     while i < length
       ans = false
       break if !self[i]
-      break !yield(self[i])
-      break pattern != nil && !(pattern === self[i]) # rubocop:disable Style/CaseEquality
+      if block_given?
+         ans = yield(self[i]) 
+         break
+      end
+      break if pattern != nil && !(pattern === self[i]) # rubocop:disable Style/CaseEquality
+
       ans = true
       i += 1
     end
     ans
   end
 
+
+ #do none first
   def m_any?(*args) # rubocop:disable Style/UnusedMethodArgument
     block_given? ? !m_all? { yield(self) } : !m_all?
   end
 end
-.
+
+puts [].m_all?
+puts [nil, true, 99].m_all?
+puts [1, 2i, 3.14].m_all?(Numeric)
+puts %w[ant bear cat].m_all?(/t/)
+puts %w[ant bear cat].m_all? { |word| word.length >= 4 }
+puts %w[ant bear cat].m_all? { |word| word.length >= 3 }
