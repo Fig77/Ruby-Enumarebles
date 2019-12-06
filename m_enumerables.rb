df1792d9fa33e1777fc1d57e48d9c718d08781a8
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-06 17:04:28 -0300

    implemented the any function. It appears to be working.

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -57,11 +57,30 @@ module Enumerable
   end
 
   # not implemented untill none.
-  def m_any?(*args) # rubocop:disable Style/UnusedMethodArgument
-    block_given? ? !m_all? { yield(self) } : !m_all?
+  def m_any?(args = nil) # rubocop:disable Style/UnusedMethodArgument
+    if block_given?
+      return true unless m_all?(nil, true) { yield(self) }
+    else
+      return true unless m_all?(args, true)
+    end
+
+    false
   end
 
-  def m_none?(*args) # rubocop:disable Style/UnusedMethodArgument
-    block_given? ? m_all?(nil, true) { yield(self) } : m_all?(nil, true)
+  def m_none?(args = nil) # rubocop:disable Style/UnusedMethodArgument
+    block_given? ? m_all?(args, true) { yield(self) } : m_all?(args, true)
   end
 end
+
+puts [1, 3.14, 42].none?(Float)
+puts [].m_none?
+puts [nil].m_none?
+puts [nil, false].m_none?
+puts [nil, false, true].m_none?
+
+puts %w[ant bear cat].m_any? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].m_any? { |word| word.length >= 4 } #=> true
+puts %w[ant bear cat].m_any?(/d/)                        #=> false
+puts [nil, true, 99].m_any?(Integer)                     #=> true
+puts [nil, true, 99].m_any?                              #=> true
+puts [].m_any?                                           #=> false
