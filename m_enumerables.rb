c586e0f0dd761cd01e831cbcd009127e483ded20
Fig77 <55416179+Fig77@users.noreply.github.com>
2019-12-10 17:25:36 -0300

    deleted some notes in code.

--- m_enumerables.rb
+++ m_enumerables.rb
@@ -126,15 +126,8 @@ module Enumerable
 
     init
   end
-
-  # Regarding points 11, 12, 13. As far as I can understand, they are redundant.
-  # 11. Creating a method  multiply_els([2,4,5]) to test my_inject is straight up using inject, but
-  #     calling the method inside this new method.
-  # 11. Modify your #my_map method to take a proc instead is also redundant since you can just pass
-  #     a proc to the original method by using &proc as an argument.
 end
 
-# Explicit point 12. (Implicit point 12 in test cases)
 def multyply_els(arr)
   arr.my_inject(:*)
 end
